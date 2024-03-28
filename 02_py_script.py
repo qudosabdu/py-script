23e3c03e0cffd079770c867973e3a0109fef9dc8
abdul qudoos <abdulqudoospk737@gmail.com>
2024-03-28 23:09:00 +0500

    solved some question

--- 02_py_script.py
+++ 02_py_script.py
@@ -9,14 +9,27 @@
 
 # if-elif-else statement
 
-x = int(input('Enter a number: '))
+# x = int(input('Enter a number: '))
 
-if x > 5:
-    print('x is greater than 5')
-elif x < 5:
-    print('x is less than 5')
+# if x > 5:
+#     print('x is greater than 5')
+# elif x < 5:
+#     print('x is less than 5')
+# else:
+#     print('x is equal to 5')
+
+
+#find the given number is prime number or not
+
+num = int(input("Enter your number: "))
+prime = True
+
+for i in range(2, num):
+    if(num % i == 0):
+        prime = False;
+        break;
+if prime:
+    print("The given number is prime")
 else:
-    print('x is equal to 5')
-
-
+    print("The given number is not Prime number")
 
