d39ed37fe976ea331e7c44cfc51ff90e04629e71
abdul qudoos <abdulqudoospk737@gmail.com>
2024-04-01 22:46:37 +0500

    praticed on classes

--- 13_py_script.py
+++ 13_py_script.py
@@ -1,11 +1,27 @@
 #OOP
-class Number:
-    def sum(self):
-        return self.a + self.b
+# class Number:
+#     def sum(self):
+#         return self.a + self.b
 
-num = Number()
-num.a = 12;
-num.b = 34
+# num = Number()
+# num.a = 12;
+# num.b = 34
 
-s = num.sum()
-print(s)
+# s = num.sum()
+# print(s)
+
+
+class Employee:
+    Company = "Google" #class variable ot Class Artibute
+
+abdul = Employee() #intance
+haris = Employee() #intance
+
+abdul.Salary = 30000 #intance variable
+haris.Salary = 28000 #intance variable
+
+# print(abdul.Company)
+# print(haris.Company)
+
+# print(abdul.Salary)
+# print(haris.Salary)
